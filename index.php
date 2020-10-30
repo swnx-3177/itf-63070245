f65781a2cb09e093526c3e550c317135baa29b64
swnx-3177 <73655917+swnx-3177@users.noreply.github.com>
2020-10-30 17:36:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,21 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello world</title>
+    <title>Hi Teacher!</title>
 </head>
 <body>
-    <h1>My First Heading</h1>
-    <p>My First Paragraph</p>
-    <img src="cat.jpg" alt="">
+    <h1>My First Project</h1>
+    <p>Sawarod Kongsamlit</p>
+    <img src="taxi.jpg">
+    <img src="cat1.jpg">
     <br>
-    <a href="https://www.it.kmitl.ac.th">This is a Link</a>
+    <a href="https://twitter.com">My Fav Link</a>
 
-    <h2>The unorderd list</h2>
-    <ul>
-       <li>Google</li>
-       <li>Yahoo</li>
-       <li>Amazon</li> 
-    </ul>
+    <p>A: Hey, man! Please call me a taxi.</p>
+    <p>B: Yes, sir. You are a taxi.</p>
+    <p>A: .....</p>
+
+
 
 </body>
 </html>
