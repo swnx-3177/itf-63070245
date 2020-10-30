58ddfa72efd057ef587b777b70441ad3d3d1202a
swnx-3177 <73655917+swnx-3177@users.noreply.github.com>
2020-10-30 17:31:59 +0700

    Create 63070245.php

---
+++ 63070245.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hi Teacher!</title>
+</head>
+<body>
+    <h1>My First Project</h1>
+    <p>Sawarod Kongsamlit</p>
+    <img src="taxi.jpg">
+    <img src="cat1.jpg">
+    <br>
+    <a href="https://twitter.com">My Fav Link</a>
+
+    <p>A: Hey, man! Please call me a taxi.</p>
+    <p>B: Yes, sir. You are a taxi.</p>
+    <p>A: .....</p>
+
+
+
+</body>
+</html>
