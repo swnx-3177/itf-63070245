6ba47e372daa66b05d8fc63db9c998631b9696d3
swnx-3177 <73655917+swnx-3177@users.noreply.github.com>
2020-10-30 17:04:28 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello world</title>
+</head>
+<body>
+    <h1>My First Project</h1>
+    <p>สวรส คงสัมฤทธิ์</p>
+    <img src="cat.jpg">
+    <br>
+    <a href="https://www.it.kmitl.ac.th">This is a Link</a>
+
+    <h2>The unorderd list</h2>
+    <ul>
+       <li>Google</li>
+       <li>Yahoo</li>
+       <li>Amazon</li> 
+    </ul>
+
+</body>
+</html>
